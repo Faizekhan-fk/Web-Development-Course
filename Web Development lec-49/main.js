613861aee027c97e8f47206da11a039d5a07f1d2
FaizE Khan <140505132+Faizekhan-fk@users.noreply.github.com>
2025-02-21 20:05:37 +0500

    Add files via upload

---
+++ Web Development lec-49/main.js
@@ -0,0 +1,31 @@
+// // const { createServer } = require('node:http');
+// // const fs = require("fs")
+
+// import http from "http"
+
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World</h1>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+
+
+// import {a, b, c} from "./mymodule"
+// console.log(a, b, c);
+
+import fk from "./mymodule.js"
+console.log(fk);
+
+
+// const a = require ("./mymodules2.js")
+// console.log(a);
